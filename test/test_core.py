effb2aae8aeaf2ab69195168de6177cea4f8b717
Almann Goo <almann.goo@gmail.com>
2023-03-20 22:09:32 -1000

    Adds some basic user session tests

--- test/test_core.py
+++ test/test_core.py
@@ -75,3 +75,68 @@ _SAMPLE_CASE = EventCase(
 )
 def test_event_parsing(case: EventCase):
     assert case.events == tuple(parse_log(case.log_lines))
+
+
+@dataclass
+class UsageCase:
+    desc: str
+    events: _coll_abc.Sequence[ChargeEvent | DisplayEvent]
+    stats: _coll_abc.Sequence[UsageSession] | Exception
+
+    def __str__(self) -> str:
+        return self.desc
+
+
+_NO_DISPLAY_USAGE_CASE = UsageCase(
+    desc="no display events",
+    events=(
+        ChargeEvent(parse_ts("2023-03-13 15:38:02 -0700"), ChargeType.AC, 100),
+        ChargeEvent(parse_ts("2023-03-13 19:18:34 -0700"), ChargeType.BATT, 36),
+    ),
+    stats=Exception("Could not find any start of AC/Battery session in log events"),
+)
+_NO_CHARGE_USAGE_CASE = UsageCase(
+    desc="no charge events",
+    events=(
+        DisplayEvent(parse_ts("2023-03-13 15:38:12 -0700"), DisplayState.OFF),
+        DisplayEvent(parse_ts("2023-03-13 15:55:02 -0700"), DisplayState.ON),
+        DisplayEvent(parse_ts("2023-03-13 16:28:05 -0700"), DisplayState.OFF),
+        DisplayEvent(parse_ts("2023-03-13 16:47:35 -0700"), DisplayState.ON),
+    ),
+    stats=Exception("Could not find any start of AC/Battery session in log events"),
+)
+_EXACTLY_ONE_OF_EACH_USAGE_CASE = UsageCase(
+    desc="one charge and one display event",
+    events=(
+        ChargeEvent(parse_ts("2023-03-13 15:38:02 -0700"), ChargeType.AC, 100),
+        DisplayEvent(parse_ts("2023-03-13 16:47:35 -0700"), DisplayState.ON),
+    ),
+    stats=Exception("Could not find any start of AC/Battery session in log events"),
+)
+# _NO_DISPLAY_BEFORE_CHARGE_SESSION_USAGE_CASE = UsageCase(
+#     desc="no display before session",
+#     events=(
+#         ChargeEvent(parse_ts("2023-03-13 15:38:02 -0700"), ChargeType.AC, 100),
+#         DisplayEvent(parse_ts("2023-03-13 16:47:35 -0700"), DisplayState.ON),
+#         ChargeEvent(parse_ts("2023-03-13 19:18:34 -0700"), ChargeType.BATT, 36),
+#     ),
+#     stats=Exception("Could not find any start of AC/Battery session in log events"),
+# )
+
+
+@pytest.mark.parametrize(
+    "case",
+    (
+        _NO_DISPLAY_USAGE_CASE,
+        _NO_CHARGE_USAGE_CASE,
+        _EXACTLY_ONE_OF_EACH_USAGE_CASE,
+        # _NO_DISPLAY_BEFORE_CHARGE_SESSION_USAGE_CASE,
+    ),
+    ids=UsageCase.__str__,
+)
+def test_usage_session(case: UsageCase):
+    try:
+        assert case.stats == tuple(calculate_usage(case.events))
+    except Exception as e:
+        assert type(case.stats) == type(e)
+        assert case.stats.args == e.args
